ca3dcaada2b0dfdd41f121a7bf5f4abef4fbd273
Herman Kolstad Jakobsen <hermankjakobsen@gmail.com>
2021-04-27 13:07:54 +0200

    Cleanup

--- robosuite/controllers/hmfc.py
+++ robosuite/controllers/hmfc.py
@@ -47,7 +47,7 @@ class HybridMotionForceController(Controller):
                         [0, 0, 0, 0, 0],
                         [0, 0, 1, 0, 0],
                         [0, 0, 0, 1, 0],
-                        [0, 0, 0, 0, 1]]).reshape([6,5])             # motion-control-subspace (x, y, ori_x, ori_y, ori_z)
+                        [0, 0, 0, 0, 1]])             # motion-control-subspace (x, y, ori_x, ori_y, ori_z)
 
         # Stiffness of the interaction [should be estimated (this one is chosen at random)]
         self.K = np.array([[1, 0, 0, 0, 0, 0],
@@ -55,7 +55,7 @@ class HybridMotionForceController(Controller):
                     [0, 0, 100, 0, 0, 0],
                     [0, 0, 0, 5, 0, 0],
                     [0, 0, 0, 0, 5, 0],
-                    [0, 0, 0, 0, 0, 1]]).reshape([6,6])
+                    [0, 0, 0, 0, 0, 1]])
 
         self.C = np.linalg.inv(self.K)
 
@@ -143,15 +143,15 @@ class HybridMotionForceController(Controller):
         return (self.z_force - self.prev_z_force) / ( 1 / self.control_freq)
 
 
-    # Fetch the psudoinverse of S_f/S_v as in equation (9.34) in chapter 9.3 of The Handbook of Robotics
+    # Fetch the psudoinverse of S_f or S_v as in equation (9.34) in chapter 9.3 of The Handbook of Robotics
     def get_S_inv(self, S, C):
-        a = np.linalg.inv(np.linalg.multi_dot([S.T,C,S]))
-        return np.array(np.linalg.multi_dot([a,S.T,C]))
+        a = np.linalg.inv(np.linalg.multi_dot([S.T, C, S]))
+        return np.array(np.linalg.multi_dot([a, S.T, C]))
 
 
     # Fetch K' as in equation (9.49) in chapter 9.3 of The Handbook of Robotics
     def get_K_dot(self, S_f, S_f_inv, C):
-        return np.array(np.linalg.multi_dot([S_f,S_f_inv,np.linalg.inv(C)])).reshape([6,6])
+        return np.linalg.multi_dot([S_f, S_f_inv, np.linalg.inv(C)])
 
 
     # Calculate the error in position and orientation (in the subspace subject to motion control)
@@ -159,54 +159,54 @@ class HybridMotionForceController(Controller):
         delta_pos = p_d - p[:2]
         delta_ori = self.quatdiff_in_euler_radians(ori, goal_ori)    
         if two_dim == True:
-            return np.array([np.append(delta_pos,delta_ori)]).reshape([5,1])
+            return np.append(delta_pos, delta_ori)
 
         else:
-            return np.append(delta_pos,delta_ori)
+            return np.append(delta_pos, delta_ori)
 
 
     # ------------  Calculation of torque -----------------
 
     # Calculate f_lambda (part of equation 9.62) as in equation (9.65) in chapter 9.3 of The Handbook of Robotics
-    def calculate_f_lambda(self, f_d_ddot, f_d_dot, f_d, S_f, C, K_Dlambda, K_Plambda, z_force):
-        S_f_inv = self.get_S_inv(S_f,C)
-        K_dot = self.get_K_dot(S_f,S_f_inv,C)
+    def calculate_f_lambda(self, f_d_ddot, f_d_dot, f_d):
+        S_f_inv = self.get_S_inv(self.S_f, self.C)
+        K_dot = self.get_K_dot(self.S_f, S_f_inv, self.C)
 
         #lambda_dot = np.linalg.multi_dot([S_f_inv, K_dot, self.J_full, self.joint_vel])
 
         lambda_dot = self.get_lambda_dot()         
         lambda_a = f_d_ddot 
-        lambda_b = np.array(np.dot(K_Dlambda,(f_d_dot-lambda_dot)))
-        lambda_c = np.dot(K_Plambda,(f_d-z_force))
+        lambda_b = np.dot(self.K_Dlambda, (f_d_dot - lambda_dot))
+        lambda_c = np.dot(self.K_Plambda, (f_d - self.z_force))
  
-        return max(lambda_a + lambda_b + lambda_c,0)
+        return max(lambda_a + lambda_b + lambda_c, 0)
 
 
     # Get the subproducts of f_lambda (for plotting/troubleshooting)
-    def get_f_lambda_subproducts(self, f_d_ddot, f_d_dot, f_d, i,time_per_iteration, S_f, C, K_Dlambda, K_Plambda, z_force, h_e_hist, jacobian, joint_names, sim):
-        S_f_inv = self.get_S_inv(S_f, C)
-        K_dot = self.get_K_dot(S_f, S_f_inv, C)
-        if sim: 
-            lambda_dot = self.get_lambda_dot()
-        else: 
-            lambda_dot = (np.linalg.multi_dot([S_f_inv,K_dot,jacobian, self.joint_vel])) # At least not correct for interaction tasks in simulation (due to incorrect readings of joint velocity)
+    def get_f_lambda_subproducts(self, f_d_ddot, f_d_dot, f_d):
+        S_f_inv = self.get_S_inv(self.S_f, self.C)
+        K_dot = self.get_K_dot(self.S_f, S_f_inv, self.C)
+        lambda_dot = (np.linalg.multi_dot([S_f_inv, K_dot, self.J_full, self.joint_vel])) # At least not correct for interaction tasks in simulation (due to incorrect readings of joint velocity)
+
         lambda_a = f_d_ddot
-        lambda_b = np.array(np.dot(K_Dlambda,(f_d_dot-lambda_dot)))
-        lambda_c = np.dot(K_Plambda,(f_d-z_force))
+        lambda_b = np.dot(self.K_Dlambda, (f_d_dot - lambda_dot))
+        lambda_c = np.dot(self.K_Plambda, (f_d - self.z_force))
         return lambda_dot, lambda_a, lambda_b, lambda_c, (lambda_a + lambda_b + lambda_c)
 
 
     # Calculate alpha_v (part of equation 9.62) as on page 213 in chapter 9.3 of The Handbook of Robotics
-    def calculate_alpha_v(self, ori, r_d_ddot, r_d_dot, p, p_d, v):
-        return (r_d_ddot.reshape([5, 1]) + np.array(np.dot(self.K_Dr, r_d_dot.reshape([5, 1]) - v)).reshape([5, 1])+ np.array(np.dot(self.K_Pr, self.get_delta_r(ori, self.goal_ori, p, p_d))).reshape([5, 1]))
+    def calculate_alpha_v(self, ori, r_d_ddot, r_d_dot, p, p_d):
+        delta_r = self.get_delta_r(ori, self.goal_ori, p, p_d)
+        return r_d_ddot + np.dot(self.K_Dr, r_d_dot - self.v) + np.dot(self.K_Pr, delta_r)
 
 
     # Calculate alpha (part of equation 9.16) as in equation (9.62) in chapter 9.3 of The Handbook of Robotics
-    def calculate_alpha(self, S_v, alpha_v,C,S_f,f_lambda):
-        S_v_inv = self.get_S_inv(S_v,C)
-        P_v = np.array(np.dot(S_v,S_v_inv))
-        C_dot = np.array(np.dot((np.identity(6)-(P_v).reshape([6,6])),C)).reshape([6,6])
-        return np.array(np.dot(S_v, alpha_v)).reshape([6,1]) + f_lambda*np.array(np.dot(C_dot,S_f)).reshape([6,1])
+    def calculate_alpha(self, alpha_v,f_lambda):
+        S_v_inv = self.get_S_inv(self.S_v, self.C)
+        P_v = np.dot(self.S_v, S_v_inv)
+        C_dot = np.dot(np.identity(6) - P_v, self.C)
+
+        return np.dot(self.S_v, alpha_v) + f_lambda * np.dot(C_dot, self.S_f).flatten()
 
 
     def set_goal(self, action):
@@ -224,7 +224,7 @@ class HybridMotionForceController(Controller):
 
         # eef measurements
         self.z_force = self.sim.data.cfrc_ext[self.probe_id][-1]
-        self.v = self.get_eef_velocity().reshape([5, 1])
+        self.v = self.get_eef_velocity()
         
         pos = self.ee_pos
         ori = T.convert_quat(T.mat2quat(self.ee_ori_mat), to="wxyz")    # (w, x, y, z) quaternion
@@ -232,16 +232,16 @@ class HybridMotionForceController(Controller):
         h_e = np.array([0, 0, self.z_force, 0, 0, 0])
 
         # control law
-        alpha_v = self.calculate_alpha_v(ori, self.r_d_ddot, self.r_d_dot, pos, self.p_d, self.v) 
-        f_lambda = self.calculate_f_lambda(self.f_d_ddot, self.f_d_dot, self.f_d, self.S_f ,self.C, self.K_Dlambda, self.K_Plambda, self.z_force)
+        alpha_v = self.calculate_alpha_v(ori, self.r_d_ddot, self.r_d_dot, pos, self.p_d) 
+        f_lambda = self.calculate_f_lambda(self.f_d_ddot, self.f_d_dot, self.f_d)
 
-        alpha = self.calculate_alpha(self.S_v, alpha_v, self.C, self.S_f, -f_lambda)
+        alpha = self.calculate_alpha(alpha_v, -f_lambda)
+
         cartesian_inertia = np.linalg.inv(np.linalg.multi_dot([self.J_full, np.linalg.inv(self.mass_matrix), self.J_full.T]))
 
         # torque computations
-        external_torque = np.dot(self.J_full.T, h_e).reshape([7,1])
-        torque = np.array(np.linalg.multi_dot([self.J_full.T ,cartesian_inertia, alpha])) + external_torque # NOTE MODIFIED FROM DEFAULT. Removed external forces and such
-        torque = torque.flatten()
+        external_torque = np.dot(self.J_full.T, h_e)
+        torque = np.linalg.multi_dot([self.J_full.T ,cartesian_inertia, alpha]) + external_torque # NOTE MODIFIED FROM DEFAULT. Removed coriolis force
 
         # update measurements
         self.prev_z_force = self.z_force
